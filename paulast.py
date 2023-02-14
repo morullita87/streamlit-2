e74f89d7dc160f6c258b790913607921a0e8ee51
morullita87 <124706089+morullita87@users.noreply.github.com>
2023-02-14 17:48:57 +0100

    Update paulast.py

--- paulast.py
+++ paulast.py
@@ -3,3 +3,20 @@ import streamlit as st
 mensaje = ":blue [azuuuul]"
 
 st.markdown(mensaje)
+
+
+opciones = ("perro","gato","oveja","serpiente")
+resultado = st.radio("animales" ,opciones)
+
+if opciones == resultado [0]
+	st.write ("woof")
+  
+if opciones == resulado [1]
+ 	st.balloons ()
+	
+if opciones == resultado [2]
+	st.write ("beeeee")
+	
+if opciones == resultado [3]
+	st.write ("ssssssssss")
+ 
