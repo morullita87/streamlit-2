a58308b9738956545575f0b7a291a4902119d29e
morullita87 <124706089+morullita87@users.noreply.github.com>
2023-02-14 17:51:30 +0100

    Update paulast.py

--- paulast.py
+++ paulast.py
@@ -8,15 +8,15 @@ st.markdown(mensaje)
 opciones = ("perro","gato","oveja","serpiente")
 resultado = st.radio("animales" ,opciones)
 
-if opciones == resultado [0]
+if opciones == resultado[0]:
 	st.write ("woof")
   
-if opciones == resulado [1]
+if opciones == resultado[1]:
  	st.balloons ()
 	
-if opciones == resultado [2]
+if opciones == resultado[2]:
 	st.write ("beeeee")
 	
-if opciones == resultado [3]
+if opciones == resultado[3]:
 	st.write ("ssssssssss")
  
