60f3a67a29743cfcc57e1dd540fe1a919d9f8477
morullita87 <124706089+morullita87@users.noreply.github.com>
2023-02-14 17:57:12 +0100

    Update paulast.py

--- paulast.py
+++ paulast.py
@@ -9,14 +9,14 @@ opciones = ("perro","gato","oveja","serpiente")
 resultado = st.radio("ANIMALES" ,opciones)
 
 if opciones == resultado[0]:
-	st.write ("woof")
+	st.write("woof")
   
 if opciones == resultado[1]:
- 	st.balloons ()
+ 	st.balloons()
 	
 if opciones == resultado[2]:
-	st.write ("beeeee")
+	st.write("beeeee")
 	
 if opciones == resultado[3]:
-	st.write ("ssssssssss")
+	st.write("ssssssssss")
  
